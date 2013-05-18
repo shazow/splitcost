d83f9ad47051a993f55c3332b4beddb69d968827
Andrey Petrov <andrey.petrov@shazow.net>
2013-05-18 01:20:56 -0700

    Worx.

--- static/js/core.js
+++ static/js/core.js
@@ -1,27 +1,30 @@
+/*
+var modPressed = false;
+
+window.addEventListener('keydown', function(e) {
+    if (e.keyCode == 16 || e.keyCode == 17) {
+        modPressed = true;
+    }
+}, false);
+
+window.addEventListener('keyup', function(e) {
+    if (e.keyCode == 16 || e.keyCode == 17) {
+        modPressed = false;
+    }
+}, false);
+*/
+
 function parseDollar(s) {
-    return Number(s);
+    return Number(s.replace('$', ''));
 };
 
 
 function CostCtrl($scope) {
+    $scope.members = [];
     $scope.expenses = [];
-    $scope.members = [
-        {
-            id: 1,
-            name: 'Andrey',
-            paid: 0.0,
-            _selected: true
-        },
-        {
-            id: 2,
-            name: 'Tracy',
-            paid: 0.0,
-            _selected: false
-        },
-    ];
 
-    $scope.avgExpense = 0.0;
-    $scope.totalExpense = 0.0;
+    $scope.avgExpense = 0;
+    $scope.totalExpense = 0;
 
     $scope.addMember = function() {
         var id = $scope.members.length + 1;
@@ -32,6 +35,7 @@ function CostCtrl($scope) {
             _selected: id == 1
         });
         $scope.memberName = '';
+        $scope.avgExpense = $scope.totalExpense / $scope.members.length;
     };
 
     $scope.getMember = function(name) {
@@ -41,14 +45,16 @@ function CostCtrl($scope) {
     };
 
     $scope.addExpense = function() {
-        var num = $scope.members.length;
-        var amount = parseDollar($scope.amount);
-        var amountEach = amount / num;
         var who = [];
         angular.forEach($scope.members, function(member) {
             if (!member._selected) return;
-            member.paid += amountEach;
             who.push(member);
+        });
+
+        var amount = parseDollar($scope.amount);
+        var amountEach = amount / who.length;
+        angular.forEach(who, function(member) {
+            member.paid += amountEach;
         });
         $scope.expenses.push({
             amount: amount,
@@ -56,6 +62,14 @@ function CostCtrl($scope) {
             who: who
         });
         $scope.totalExpense += amount;
-        $scope.avgExpense = $scope.totalExpense / num;
+        $scope.avgExpense = $scope.totalExpense / $scope.members.length;
+    };
+
+    $scope.getNames = function(members) {
+        var names = [];
+        angular.forEach(members, function(member) {
+            names.push(member.name);
+        });
+        return names;
     };
 };
